bec5be57049ad5a0ebf1f86ed74b05b6d867b01f
usman-ghani564 <usmanghani564.ug9@gmail.com>
2024-11-28 01:13:26 +0500

    add: postgres setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,23 @@
-use actix_web::{middleware, web, App, HttpServer, Responder};
+use actix_web::{
+    middleware,
+    web::{self, Data},
+    App, HttpServer, Responder,
+};
 use dotenv::dotenv;
 use log::info;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::env;
 
-async fn health_check() -> impl Responder {
-    "Backend is running!"
+async fn health_check(pool: Data<PgPool>) -> impl Responder {
+    if sqlx::query("SELECT 1")
+        .execute(pool.get_ref())
+        .await
+        .is_ok()
+    {
+        "Database is connected!"
+    } else {
+        "Database connection failed!"
+    }
 }
 
 #[actix_web::main]
@@ -17,10 +30,19 @@ async fn main() -> std::io::Result<()> {
         .parse()
         .expect("PORT must be a valid number");
 
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create database connection pool");
+
     info!("Starting server on port {}", port);
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(Data::new(pool.clone()))
             .wrap(middleware::Logger::default())
             .route("/health", web::get().to(health_check))
     })
