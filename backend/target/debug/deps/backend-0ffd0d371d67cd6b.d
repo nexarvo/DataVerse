fa07f17a6e2b55dae779c87f65dd4623fd90d760
Usman Ghani <53095363+nexarvo@users.noreply.github.com>
2024-11-28 00:50:42 +0500

    Delete backend/target directory

--- backend/target/debug/deps/backend-0ffd0d371d67cd6b.d
+++
@@ -1,5 +0,0 @@
-/Users/usmanghani/Desktop/Personal Projects/DataVerse/backend/target/debug/deps/libbackend-0ffd0d371d67cd6b.rmeta: src/main.rs
-
-/Users/usmanghani/Desktop/Personal Projects/DataVerse/backend/target/debug/deps/backend-0ffd0d371d67cd6b.d: src/main.rs
-
-src/main.rs:
--- backend/target/debug/deps/backend-46365d472d69ab78.d
+++
@@ -1,5 +0,0 @@
-/Users/usmanghani/Desktop/Personal Projects/DataVerse/backend/target/debug/deps/libbackend-46365d472d69ab78.rmeta: src/main.rs
-
-/Users/usmanghani/Desktop/Personal Projects/DataVerse/backend/target/debug/deps/backend-46365d472d69ab78.d: src/main.rs
-
-src/main.rs:
